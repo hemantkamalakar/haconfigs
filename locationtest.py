99de64df9eba354c9ed63304d4ce421bd50fa48b
Hemant Kamalakar <hemant.kamalakar@gmail.com>
2019-01-05 16:44:08 +0530

    Delete locationtest.py

--- locationtest.py
+++
@@ -1,4 +0,0 @@
-from locationsharinglib import Service
-service = Service("pranav.kamalakar@gmail.com", "Soham2018", cookies_file=".google_maps_location_sharing.cookies")
-for person in service.get_all_people():
-    print(person)
